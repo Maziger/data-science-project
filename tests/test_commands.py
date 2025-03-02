8ad6b9b1c6737839235fb01954f8dbccad31a5f5
Maziger <rasbrogger@gmail.com>
2025-03-02 17:52:26 +0100

    Added new script

---
+++ tests/test_commands.py
@@ -0,0 +1,16 @@
+my_string = "Hello World"
+print(my_string)
+
+# Return type of the variable
+type(my_string)
+
+# Length of the string
+len(my_string)
+
+# Return identity of an object
+id(my_string)
+
+# Use of help function
+help
+
+
